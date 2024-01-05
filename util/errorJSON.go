bc781f3becdc430130ee0bbf7b7ff2e4d84a45f7
jayphan14 <kha21052004@gmail.com>
2024-01-04 23:37:36 -0500

    json error utils;

--- util/errorJSON.go
+++ util/errorJSON.go
@@ -5,3 +5,7 @@ import "github.com/gin-gonic/gin"
 func ErrorResponse(err error) gin.H {
 	return gin.H{"error": err.Error()}
 }
+
+func ErrorResponseString(err string) gin.H {
+	return gin.H{"error": err}
+}
